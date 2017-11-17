14a78d834f3a4d2e27adf9fcd730d6c8dfcb672c
Joshua Randall <josh.randall16@gmail.com>
2017-11-17 02:13:24 -0500

    Scripts Created!
    Created a Gridspace script
    -This will reference the buttons in the Unity Editor
    -Each button will reference the GameController script
    
    GameController
    -This Script will manage the game itself
    -It will reference and manage all the buttons i on the TicTacToe board

---
+++ Assets/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text[] buttonList; // Create an array for all of the buttons text's
+							  //We want to check the buttons text to see if there is 3 matching values in a row
+
+
+
+	void Awake()
+	{
+		SetGameControllerReferenceOnButtons (); // When the game starts, this function will be called
+	}
+
+
+
+	public void SetGameControllerReferenceOnButtons()
+	{
+		//Loop through the buttonList
+		for (int i = 0; i < buttonList.Length; i++) 
+		{
+			buttonList [i].GetComponentInParent<GridSpace> ().SetGameControllerReference (this);
+			// We are setting the "GridSpace" which is the parent object to now reference 
+			// "this" GameController Script
+
+			
+		}
+	}
+
+
+
+	public string GetPlayerSide()
+	{
+		return "?";
+	}
+
+	public void EndTurn()
+	{
+		Debug.Log ("End Turn is not implemented!!");
+	}
+
+}
---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour {
+
+
+	//Declared Variables
+	public Button button;
+	public Text buttonText;
+
+
+	private GameController gameController;
+
+
+	public void SetGameControllerReference(GameController controller)
+	{
+		gameController = controller; // Setting gameController to the ref controller
+	}
+
+	public void SetSpace()
+	{
+		buttonText.text = gameController.GetPlayerSide (); // Assign the text property
+		button.interactable = false; // Making the button non-interactable
+		gameController.EndTurn();
+	}
+
+
+
+
+
+}
